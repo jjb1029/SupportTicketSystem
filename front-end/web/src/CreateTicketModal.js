fd82e57e4f01d6351f7819170c627821e8300d82
Jorden Burchick <31428342+jjb1029@users.noreply.github.com>
2025-05-26 21:02:40 -0400

    Updated CreateTicketModal
    Created the ticket creation modal, little popup window to enter ticket title and ticket description. Also can't submit ticket unless both fields have been entered

--- front-end/web/src/CreateTicketModal.js
+++ front-end/web/src/CreateTicketModal.js
@@ -1,6 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const CreateTicketModal = ({onClose}) => {
+    const[ticketTitle, setTicketTitle] = useState('');
+    const[ticketDescription, setTicketDescription] = useState('')
+
+    const handleCreateTicket = async(e) => {
+        e.preventDefault();
+
+        // check for ticket in local storage
+        const token = localStorage.getItem('token');
+
+        // if we don't find token, we let them know
+        if(!token) {
+            alert("You must be logged in to create a ticket.");
+            return;
+        }
+
+        // if there is a token, try to send a POST request to the backend for ticket creation
+        try {
+            const response = await fetch('http://localhost:8080/api/tickets', {
+                method: 'POST', // used to send data to server
+                headers: { // key-value pairs that give server information about the request, important so spring boot knows how to parse the data
+                    'Content-type':'application/json', // tells the server we're sending json
+                    'Authorization': `Bearer ${token}`, // securely sends a token to our back end
+                },
+                body: JSON.stringify({
+                    title: ticketTitle, // contains the actual data, converted to json string
+                    description: ticketDescription
+                }),
+            });
+
+            // if response works
+            if(response.ok) {
+                const data = await response.json(); // converts response from backend JSON to JS object and holds Ticket object
+                alert(`Ticket #${data.ticketNo} created!`);
+                setTicketTitle('');
+                setTicketDescription('');
+                onClose();
+            } else {
+                alert('Failed to create ticket');
+            }
+        } catch(error) {
+            console.log('Error: ' + error);
+            alert('An error occured while creating ticket');
+        }
+    }
+
     return (
         <div style= {{
             position: 'fixed',
@@ -14,7 +59,38 @@ const CreateTicketModal = ({onClose}) => {
             opacity: .7,
         }}>
             <h2>Create Ticket</h2>
-            <p>This is a placeholder modal</p>
+            <p style= {{
+                marginBottom: '1px'
+            }}>Ticket Title</p>
+            <input type="text"
+                value={ticketTitle}
+                onChange={(e) => setTicketTitle(e.target.value)}
+                placeholder="Ticket Title"
+                required
+                style = {{
+                    marginTop: '0px'
+                }}
+            />
+            <br />
+            <p style = {{
+                marginBottom: '1px'
+            }}>Ticket Description</p>
+            <textarea
+                value={ticketDescription}
+                onChange={(e) => setTicketDescription(e.target.value)}
+                placeholder="Describe your issue..."
+                required
+                style = {{
+                    marginTop: '10px'
+                }}
+            />
+            <br />
+            <button 
+                type="submit" 
+                onClick={handleCreateTicket}
+                disabled={!ticketDescription || !ticketTitle}>
+            Submit Ticket
+            </button>
             <button onClick={onClose}>Close</button>
         </div>
     );
